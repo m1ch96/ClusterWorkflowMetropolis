324e8761013c47b2f423fd60608e881abc8f4238
micheleproietto <micheleproietto96@gmail.com>
2023-04-26 14:35:08 +0200

    Code for workstation and definition of main

--- Transpose.cpp
+++ Transpose.cpp
@@ -12,8 +12,8 @@ using std:: vector;
 //la dimensione della matrice deve essere un multiplo del numero di processori; parto con 4 processori e una matrice (8X8)
 
 
-void Transpose(int rank,int h);
-int** MatrixGeneration(int N);
+void Transpose(int** Matrix,int rank,int size,const int N);
+
 
 
 int main(int argc, char ** argv)
@@ -21,8 +21,9 @@ int main(int argc, char ** argv)
     int rank;
     int size;
     const int N = 8;
-    
+    int** MatrixGeneration(int N);
     int** Matrix = MatrixGeneration(N);
+    
 
     MPI::Init(argc, argv);
     rank = MPI::COMM_WORLD.Get_rank();
@@ -33,15 +34,22 @@ int main(int argc, char ** argv)
          cout << "Error!: you are running a serial code" << endl;
          MPI::COMM_WORLD.Abort(1);
     }
-    else if ((N % size) != 0 && rank == 0)
+    else if ((N % size) != 0)
     {
-        cout << "Error!: Matrix dimension should be a multiple of process number" << endl;
+        if (rank == 0)
+        {
+            cout << "Error!: Matrix dimension should be a multiple of process number" << endl;
+        }
+                
         MPI::COMM_WORLD.Abort(1);
     }
-    
-    
+    else 
+    {
+        
+        Transpose(Matrix, rank,size,N);   
 
-    //Transpose(rank,h, x);   
+    }
+
     MPI::Finalize();
     return 0;
 }
@@ -74,14 +82,57 @@ int** MatrixGeneration(int N)
         cout << endl;
     }
     
-
-
     return arr;
 }
 
-void Transpose(int rank,int h) 
+void Transpose(int** Matrix,int rank,int size,const int N) 
 {    
+    int Rapp =  N /size ;
 
-    
+    int** LoMat = new int* [N];
+
+    for (int i = 0; i < N; i++)
+    {
+        LoMat[i] = new int[N];
+    }
+
+    for (int proc=0; proc<size; proc++) 
+    {
+        if (proc == rank) 
+        {
+            cout << rank << endl;
+           cout << "il rank è: " << rank << endl;
+           if (rank == 0)
+           {
+              for (int i = 0; i <Rapp ; i++)
+              {
+                for (int j = 0; j < N ; j++)
+                {
+                 LoMat[i][j] = Matrix[i][j];
+                 cout <<  LoMat[i][j] << " ";                       
+                }
+                cout <<  endl;    
+              }
+              cout <<  endl;
+           }
+           else
+           {
+              for (int i = 0; i <Rapp ; i++)
+              {  
+                 for (int j = 0; j < N; i++)
+                 {
+                   LoMat[i][j] = Matrix[i*Rapp][j];
+                   cout <<  LoMat[i][j] << " ";                         
+                 }
+               cout <<  endl;   
+              }
+             cout <<  endl;
+           }
+           
+
+        }
+        //MPI_Barrier(MPI_COMM_WORLD);
+    }
+
 }
 
